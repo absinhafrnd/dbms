db398e45fb805c4b89cd5820db86359162265a80
Abhijeet Sinha <43959706+absinhafrnd@users.noreply.github.com>
2018-10-09 03:36:18 +0530

    Add files via upload

---
+++ dbms project/sample php/new2.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Dashboard</title>
+	<link rel="stylesheet" href="style1.css">
+	<link rel="stylesheet" href="<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+</head>
+<body>
+	<div class="middle">
+	<div class="menu">
+		<li class="item">
+			<a href="" class="btn"><i class="fas fa-user-circle"></i>Profile</a>
+			 <div clas="smenu"> 
+			 	<a href="#">Faculty</a>
+			 	<a href="#">Students</a>
+
+			 </div>
+			</li>
+			<li class="item">
+			<a href="" class="btn"><i class="fas fa-address-book"></i>Attendance</a>
+			 <div clas="smenu"> 
+			 	<a href="#">Subject</a>
+			 	<a href="#">Holidays</a>
+			 </div>
+			</li>
+			<li class="item">
+			<a href="" class="btn"><i class="fab fa-accusoft"></i>Courses</a>
+			 <div clas="smenu"> 
+			 	<a href="#">B.E.</a>
+			 	<a href="#">M.E.</a>
+
+			 </div>
+			</li>
+			<li class="item">
+			<a href="" class="btn">Setting</a>
+			 <div clas="smenu"> 
+			 	<a href="#"><i class="fas fa-cog"></i>Admin panel</a>
+			 	<a href="#">About</a>
+
+			 </div>
+			</li>
+			<li class="btn" href="#"><i class="fas fa-sign-out-alt"></i>logout</a>
+			</li>
+		</div>
+	</div>
+	</body>
+</html>
+
+</html> 
---
+++ dbms project/sample php/test3.php
@@ -0,0 +1,32 @@
+<html>
+	<link href='style3.css' rel='stylesheet'>
+	<ul>
+		<li><a>Home</a></li>
+		<li><a>Profile</a>
+			<ul>
+				<li><a>Student</a></li>
+				<li><a>Faculty</a></li>
+			</ul>
+		</li>
+		<li><a>Attendance</a>
+			<ul>
+				<li><a>Subjects</a></li>
+				<li><a>Holidays</a></li>
+			</ul>
+		</li>
+		<li><a>Courses</a>
+			<ul>
+				<li><a>B.E.</a></li>
+				<li><a>M.E.</a></li>
+			</ul>
+		</li>
+		<li><a>Settings</a>
+			<ul>
+				<li><a>Admin panel</a></li>
+				
+			</ul>
+		</li>
+		<li><a>About</a></li>
+		<li><a>Logout</a></li>	
+	</ul>
+</html>
